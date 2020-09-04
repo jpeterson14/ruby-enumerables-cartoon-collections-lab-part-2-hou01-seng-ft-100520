bff06cc7295c581a3de78c5272f74cb4c650939d
Jake Peterson <jpeterson0514@gmail.com>
2020-09-04 21:07:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |string|string.upcase|
 end
 end 
-end
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
@@ -23,4 +22,5 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
+end
 end
