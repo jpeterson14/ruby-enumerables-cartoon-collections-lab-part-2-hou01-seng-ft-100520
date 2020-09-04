32c69f9b3a838eeb6c92d5a09c71caa82345c2e7
Jake Peterson <jpeterson0514@gmail.com>
2020-09-04 21:32:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   empty_array = []
   planeteer_calls.map do |element|
-     element == valid_calls
+     element = valid_calls
       element << empty_array
 end
 end
